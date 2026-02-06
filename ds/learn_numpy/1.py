7a4abcd989fbe9d02cbb6cc50528ec6cad10ef21
Ayush Singh <mm0954@maven.local>
2026-02-06 21:53:41 +0530

    Fix folder structure

---
+++ ds/learn_numpy/1.py
@@ -0,0 +1,3 @@
+squares = lambda num: {x: x**3 for x in range(1, num + 1)}
+
+print(squares(10))
---
+++ ds/learn_numpy/2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+data = [32, 31, 24, 36, 32, 28, 24]
+temperatures = np.array(data)
+
+avg = np.mean(temperatures)
+
+print(avg)
